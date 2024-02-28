e3d0bf06d83a0a51022afdae716fba20e0e108ef
Madhav314 <mdhimal314@gmail.com>
2024-02-27 22:19:34 -0600

    made attack work

--- player.gd
+++ player.gd
@@ -4,11 +4,18 @@ extends CharacterBody2D
 
 @onready var face = "down"
 
+var attacking: bool = false
+
 func _physics_process(delta):
 
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
-	if(Input.is_action_pressed("move_left")):
+	if(Input.is_action_pressed("attack") or attacking):
+		attacking = true
+		_animated_sprite.play("attack_" + face)
+		if(_animated_sprite.frame == 3):
+			attacking = false
+	elif(Input.is_action_pressed("move_left")):
 		_animated_sprite.play("running_left")
 		face = "left"
 	elif(Input.is_action_pressed("move_right")):
@@ -20,8 +27,6 @@ func _physics_process(delta):
 	elif(Input.is_action_pressed("move_down")):
 		_animated_sprite.play("running_down")
 		face = "down"
-	elif(Input.is_action_pressed("attack")):
-		_animated_sprite.play("attack_" + face)
 	else:
 		_animated_sprite.play("idle_" + face)
 	

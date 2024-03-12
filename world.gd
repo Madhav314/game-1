cb5d6ce09f5828017d210e46b02beb44519e91df
Madhav314 <mdhimal314@gmail.com>
2024-03-12 12:22:25 -0500

    menu

--- world.gd
+++ world.gd
@@ -5,12 +5,15 @@ extends Node2D
 func _ready():
 	health.set_max_health(5)
 
+func _physics_process(delta):
+	if(Input.is_action_pressed("switch")):
+		get_tree().change_scene_to_file("res://control.tscn")
+
 func _spawn():
 	var bat = preload("res://bat.tscn").instantiate()
 	%PathFollow2D.progress_ratio = randf()
 	bat.global_position = %PathFollow2D.global_position
 	add_child(bat)
-
 
 func _on_timer_timeout():
 	_spawn()
